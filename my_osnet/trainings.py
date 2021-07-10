9d1320e6553de101e1aba61845933e392a44693f
hossein <hossein.bodaghi74@gmail.com>
2021-07-10 19:57:06 +0430

    one train function with the name of train_collection_id2 has been added and difference is on loss functions for body color and foot color so it takes three criterions as input

--- my_osnet/trainings.py
+++ my_osnet/trainings.py
@@ -501,6 +501,249 @@ def train_collection_id(num_epoch,
                 print('our net improved')
                 torch.save(attr_net, saving_path+'attrnet_'+version+'_epoch'+str(epoch)+'.pth')
                 torch.save(optimizer.state_dict(), saving_path+'optimizer_'+version+'_epoch'+str(epoch)+'.pth')
+#%%
+def train_collection_id2(num_epoch,
+                     attr_net,
+                     train_loader,
+                     test_loader,
+                     optimizer,
+                     scheduler,
+                     criterion1,
+                     criterion2,
+                     criterion3,
+                     saving_path,
+                     num_id,
+                     device,
+                     version,
+                     resume=False,
+                     loss_train = None,
+                     loss_test=None,
+                     train_F1=None,
+                     test_F1=None,
+                     stop_epoch=None): 
+    # model_output (tuple): (out_head,out_body,out_body_type,out_leg,out_foot,out_gender,out_bags,out_body_colour,out_leg_colour,out_foot_colour,out_id)
+    # loader_outpu (tuple): (img,id,head,body,body_type,leg,foot,gender,bags,body_colour,leg_colour,foot_colour)
+    print('this is start')
+    if resume:
+        train_loss = loss_train
+        test_loss = loss_test
+        F1_train = train_F1
+        F1_test = test_F1
+    else:
+        train_loss = []
+        test_loss = []
+        F1_train = []
+        F1_test = []
+    # gc.collect()
+    # torch.cuda.empty_cache()
+    # attr_net = attr_net.to(device)
+    print('epoches started')
+    if resume:
+        start_epoch = stop_epoch+1
+    else:
+        start_epoch = 1    
+    for epoch in range(start_epoch,num_epoch+1):
+        
+        torch.cuda.empty_cache()
+        attr_net = attr_net.to(device)        
+        attr_net.train()
+        loss_e = []
+        loss_t = []
+        ft_train = []
+        ft_test = []
+        
+        for idx, data in enumerate(train_loader):
+            
+            # forward step
+            optimizer.zero_grad()
+            out_data = attr_net.forward(data[0].to(device))
+            
+            for i in range(len(data)):
+                data[i] = data[i].to(device)
+            # compute losses and evaluation metrics:
+                
+            # head 
+            loss0 = criterion1(out_data[0],data[2].argmax(dim=1))        
+            y = tensor_max(out_data[0])
+            metrics = tensor_metrics(data[2].float(),y)
+            ft_train.append(metrics[7])
+            
+            # body
+            loss1 = criterion1(out_data[1],data[3].argmax(dim=1))
+            y = tensor_max(out_data[1])
+            metrics = tensor_metrics(data[3].float(),y)
+            ft_train.append(metrics[7])
+            
+            # body type
+            loss2 = criterion2(out_data[2].squeeze(),data[4].float())    
+            y = tensor_thresh(out_data[2], 0.5)
+            metrics = boolian_metrics(data[4].float(),y)
+            ft_train.append(metrics[3])
+            
+            # leg
+            loss3 = criterion1(out_data[3],data[5].argmax(dim=1))
+            y = tensor_max(out_data[3])
+            metrics = tensor_metrics(data[5].float(),y)
+            ft_train.append(metrics[7])
+            
+            # foot 
+            loss4 = criterion1(out_data[4],data[6].argmax(dim=1))      
+            y = tensor_max(out_data[4])
+            metrics = tensor_metrics(data[6].float(),y)  
+            ft_train.append(metrics[7])
+            
+            # gender
+            loss5 = criterion2(out_data[5].squeeze(),data[7].float())
+            y = tensor_thresh(out_data[5], 0.5)
+            metrics = boolian_metrics(data[7].float(),y)  
+            ft_train.append(metrics[3])
+            
+            # bags
+            loss6 = criterion1(out_data[6],data[8].argmax(dim=1))
+            y = tensor_max(out_data[6])
+            metrics = tensor_metrics(data[8].float(),y)
+            ft_train.append(metrics[7])
+            
+            # body colour
+            loss7 = criterion3(out_data[7],data[9])      
+            y = tensor_thresh(out_data[7], 0.5)
+            metrics = tensor_metrics(data[9].float(),y)
+            ft_train.append(metrics[7])
+            
+            # leg colour
+            loss8 = criterion1(out_data[8],data[10].argmax(dim=1))
+            y = tensor_max(out_data[8])
+            metrics = tensor_metrics(data[10].float(),y)
+            ft_train.append(metrics[7])
+            
+            # foot colour
+            loss9 = criterion3(out_data[9],data[11])
+            y = tensor_max(out_data[9])
+            metrics = tensor_metrics(data[11].float(),y)      
+            ft_train.append(metrics[7])
+            
+            # id
+            loss10 = criterion1(out_data[-1],data[1].argmax(dim=1))
+            y = tensor_max(out_data[-1])
+            metrics = tensor_metrics(data[1].float(),y)      
+            ft_train.append(metrics[7])            
+            
+            # total loss
+            loss = loss0+loss1+loss2+loss3+loss4+loss5+loss6+loss7+loss8+loss10
+            loss_e.append(loss.item())
+            
+            # backward step
+            loss.backward()
+            
+            # optimization step
+            optimizer.step()
+            
+            # print log
+            if idx % 1 == 0:
+                print('Train Epoch: {} [{}/{} , lr {}] \t Loss: {:.6f} , F1: {:.3f}'.format(
+                    epoch, idx , len(train_loader),
+                     optimizer.param_groups[0]['lr'],
+                      loss.item(),np.mean(ft_train)))
+       
+        train_loss.append(np.mean(loss_e))
+        F1_train.append(np.mean(ft_train))
+        torch.save(train_loss,saving_path+'trainloss_'+version+'.pth')
+        torch.save(F1_train,saving_path+'trainF1_'+version+'.pth')
+        
+        # evaluation:     
+        attr_net.eval()
+        with torch.no_grad():
+            for idx, data in enumerate(test_loader):
+                
+                # data = data.to(device) 'list' object has no attribute 'to'
+                # out_data = attr_net.predict(data[0].to(device))
+                out_data = attr_net(data[0].to(device))
+                for i in range(len(data)):
+                    data[i] = data[i].to(device)
+                # compute losses and evaluation metrics:
+                    
+                # head 
+                loss0 = criterion1(out_data[0],data[2].argmax(dim=1))        
+                y = tensor_max(out_data[0])
+                metrics = tensor_metrics(data[2].float(),y)
+                ft_test.append(metrics[7])
+                
+                # body
+                loss1 = criterion1(out_data[1],data[3].argmax(dim=1))
+                y = tensor_max(out_data[1])
+                metrics = tensor_metrics(data[3].float(),y)
+                ft_test.append(metrics[7])
+                
+                # body type
+                loss2 = criterion2(out_data[2].squeeze(),data[4].float())    
+                y = tensor_thresh(out_data[2], 0.5)
+                metrics = boolian_metrics(data[4].float(),y)
+                ft_test.append(metrics[3])
+                
+                # leg
+                loss3 = criterion1(out_data[3],data[5].argmax(dim=1))
+                y = tensor_max(out_data[3])
+                metrics = tensor_metrics(data[5].float(),y)
+                ft_test.append(metrics[7])
+                
+                # foot 
+                loss4 = criterion1(out_data[4],data[6].argmax(dim=1))      
+                y = tensor_max(out_data[4])
+                metrics = tensor_metrics(data[6].float(),y)  
+                ft_test.append(metrics[7])
+                
+                # gender
+                loss5 = criterion2(out_data[5].squeeze(),data[7].float())
+                y = tensor_thresh(out_data[5], 0.5)
+                metrics = boolian_metrics(data[7].float(),y)  
+                ft_test.append(metrics[3])
+                
+                # bags
+                loss6 = criterion1(out_data[6],data[8].argmax(dim=1))
+                y = tensor_max(out_data[6])
+                metrics = tensor_metrics(data[8].float(),y)
+                ft_test.append(metrics[7])
+                
+                # body colour
+                loss7 = criterion3(out_data[7],data[9])      
+                y = tensor_thresh(out_data[7], 0.5)
+                metrics = tensor_metrics(data[9].float(),y)
+                ft_test.append(metrics[7])
+                
+                # leg colour
+                loss8 = criterion1(out_data[8],data[10].argmax(dim=1))
+                y = tensor_max(out_data[8])
+                metrics = tensor_metrics(data[10].float(),y)
+                ft_test.append(metrics[7])
+                
+                # foot colour
+                loss9 = criterion3(out_data[9],data[11])
+                y = tensor_max(out_data[9])
+                metrics = tensor_metrics(data[11].float(),y)      
+                ft_test.append(metrics[7])
+
+                # id
+                loss10 = criterion1(out_data[-1],data[1].argmax(dim=1))
+                y = tensor_max(out_data[-1])
+                metrics = tensor_metrics(data[1].float(),y)      
+                ft_train.append(metrics[7])          
+                
+                # total loss
+                loss = loss0+loss1+loss2+loss3+loss4+loss5+loss6+loss7+loss8+loss9
+                loss_t.append(loss.item())
+        test_loss.append(np.mean(loss_t))
+        F1_test.append(np.mean(ft_test))
+        print('Epoch: {}\ntrain loss: {:.6f}\ntest loss: {:.6f}\n\nF1 train: {:.4f}\nF1 test: {:.4f} '.format(
+                    epoch,train_loss[-1],test_loss[-1],F1_train[-1],F1_test[-1]))
+        
+        torch.save(test_loss,saving_path+'testloss_'+version+'.pth')
+        torch.save(F1_test,saving_path+'testF1_'+version+'.pth')
+        scheduler.step()
+        if len(F1_test)>2: 
+            if F1_test[-1] > F1_test[-2]:
+                print('our net improved')
+                torch.save(attr_net, saving_path+'attrnet_'+version+'_epoch'+str(epoch)+'.pth')
+                torch.save(optimizer.state_dict(), saving_path+'optimizer_'+version+'_epoch'+str(epoch)+'.pth')
 
 
 #%%    
